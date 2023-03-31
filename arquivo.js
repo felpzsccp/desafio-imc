d8245754c9e45f46fb9f96b31127860c33878b13
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2023-03-31 19:37:40 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -16,19 +16,19 @@ if (imc < 16) {
 } else if(imc >= 16 && imc <= 16.99){
     classificacao = 'Baixo peso grave';
 
-} else if(imc >= 17 && imc <= 18,49){
+} else if(imc >= 17 && imc <= 18.49){
     classificacao = 'Baixo peso';
 
-} else if(imc >= 18,50 && imc <= 24,99){
+} else if(imc >= 18,50 && imc <= 24.99){
     classificacao = 'Peso normal'
 
-} else if(imc >= 25 && imc <= 29,99){
+} else if(imc >= 25 && imc <= 29.99){
     classificacao = 'Sobrepeso'
 
-} else if(imc >= 30 && imc <= 34,99){
+} else if(imc >= 30 && imc <= 34.99){
     classificacao = 'Obesidade grau I'
 
-} else if(imc >= 35 && imc <= 39,99){
+} else if(imc >= 35 && imc <= 39.99){
     classificacao = 'Obesidade grau II'
 
 } else {(imc < 40 )
